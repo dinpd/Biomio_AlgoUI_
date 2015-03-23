3c5855390c5171373860269af5bdd3e84dae8e21
Vitalius Parubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-03-23 20:10:45 +0200

    Added matches drawing function

--- algorithms/cvtools/visualization.py
+++ algorithms/cvtools/visualization.py
@@ -8,6 +8,7 @@ Implementation of functions for image drawing and visualization based on OpenCV.
     draw - draw some object or objects on image and return image object;
 """
 from matplotlib import pyplot as plt
+import scipy as sp
 import random
 import cv2
 
@@ -69,6 +70,41 @@ def showClusters(clusters, image):
     plt.show()
 
 
+def showMatches(imgobj1, imgobj2, matches, key='data'):
+    h1, w1 = imgobj1[key].shape[:2]
+    h2, w2 = imgobj2[key].shape[:2]
+    view = sp.zeros((max(h1, h2), w1 + w2, 3), sp.uint8)
+    view[:h1, :w1] = imgobj1[key]
+    view[:h2, w1:] = imgobj2[key]
+    view[:, :, 1] = view[:, :, 0]
+    view[:, :, 2] = view[:, :, 0]
+
+    for knn in matches:
+        for match in knn:
+            # print m.queryIdx, m.trainIdx, m.distance
+            color = tuple([sp.random.randint(0, 255) for _ in xrange(3)])
+            print "=================="
+            print color
+            print "(" + str(int(imgobj1['keypoints'][match.queryIdx].pt[0])) + ", " + \
+                  str(int(imgobj1['keypoints'][match.queryIdx].pt[1])) + ") (" + \
+                  str(int(imgobj2['keypoints'][match.trainIdx].pt[0])) + ", " + \
+                  str(int(imgobj2['keypoints'][match.trainIdx].pt[1])) + ") :" + str(match.distance)
+            dx = int(imgobj1['keypoints'][match.queryIdx].pt[0]) - int(imgobj2['keypoints'][match.trainIdx].pt[0])
+            dy = int(imgobj1['keypoints'][match.queryIdx].pt[1]) - int(imgobj2['keypoints'][match.trainIdx].pt[1])
+            print dx
+            print dy
+            print pow(pow(dx, 2) + pow(dy, 2), 0.5)
+            print "=================="
+            cv2.line(view, (int(imgobj1['keypoints'][match.queryIdx].pt[0]),
+                            int(imgobj1['keypoints'][match.queryIdx].pt[1])),
+                     (int(imgobj2['keypoints'][match.trainIdx].pt[0] + w1),
+                      int(imgobj2['keypoints'][match.trainIdx].pt[1])), color)
+    # plt.imshow(view)
+    # plt.show()
+    cv2.imshow("view", view)
+    cv2.waitKey()
+
+
 def drawRectangle(image, rect, color):
     """
     Open CV Tools/Visualization Module
@@ -121,3 +157,23 @@ def drawClusters(clusters, image):
                                               random.randint(0, 255),
                                               random.randint(0, 255)))
     return out
+
+
+def drawMatches(imgobj1, imgobj2, matches):
+    h1, w1 = imgobj1['data'].shape[:2]
+    h2, w2 = imgobj2['data'].shape[:2]
+    view = sp.zeros((h1 + h2, w1 + w2, 3), sp.uint8)
+    view[:h1, :w1, 3] = imgobj1['data']
+    view[h1:, w1:, 3] = imgobj2['data']
+    view[:, :, 1] = view[:, :, 0]
+    view[:, :, 2] = view[:, :, 0]
+
+    for knn in matches:
+        for match in knn:
+            # print m.queryIdx, m.trainIdx, m.distance
+            color = tuple([sp.random.randint(0, 255) for _ in xrange(3)])
+            cv2.line(view, (int(imgobj1['keypoints'][match.queryIdx].pt[0]),
+                            int(imgobj1['keypoints'][match.queryIdx].pt[1])),
+                     (int(imgobj2['keypoints'][match.trainIdx].pt[0] + w1),
+                      int(imgobj2['keypoints'][match.trainIdx].pt[1] + h1)), color)
+    return view
