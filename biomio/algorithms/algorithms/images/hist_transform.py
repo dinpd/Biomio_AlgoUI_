2525b356e0f00b8788b8497c6fb3e97eeb170329
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2016-08-09 17:10:35 +0300

    Modified color normalization method

--- biomio/algorithms/algorithms/images/hist_transform.py
+++ biomio/algorithms/algorithms/images/hist_transform.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def histtruncate(img, low, high):
     h = len(img)
     w = 0
@@ -19,3 +20,20 @@ def histtruncate(img, low, high):
             elif img[i][j] > high_value:
                 img[i][j] = high_value
     return img, low_value, high_value
+
+
+def color_normalization(image, normal=(127, 127, 127)):
+    avg = [0.0, 0.0, 0.0]
+    for inx in range(0, image.shape[0], 1):
+        for jnx in range(0, image.shape[1], 1):
+            avg += image[inx, jnx]
+    avg /= (1.0 * image.shape[0] * image.shape[1])
+    coeff = normal / avg
+    img = image.copy()
+    for inx in range(0, image.shape[0], 1):
+        for jnx in range(0, image.shape[1], 1):
+            if len(image.shape) > 2:
+                img[inx, jnx] *= coeff
+            else:
+                img[inx, jnx] *= coeff[0]
+    return img
