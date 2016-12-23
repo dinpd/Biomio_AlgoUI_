175661db3591ea53490dc2d911ff1363068d0c75
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2016-12-23 19:37:35 +0200

    [Refactoring] Removed scripts used in Core Algorithms

--- biomio/algorithms/cascades/rectfilter.py
+++
@@ -1,6 +0,0 @@
-def filterRectangles(rects):
-    if len(rects) == 0:
-        return rects
-    if len(rects) == 1:
-        return rects[0]
-    return reduce(lambda p, q: q if q[2] > p[2] and q[3] > p[3] else p, rects, [0]*4)
--- biomio/algorithms/cvtools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-OpenCV Tools Module
-Implementation of functions for basic processing of images based on OpenCV.
-"""
-from types import (listToNumpy_ndarray, numpy_ndarrayToList,
-                   iplImageToNumpy_darray, numpy_darrayToIplImage, classKeyPointToArray)
-from effects import equalizeHist, grayscale, grayscaleAndEqualize, rotate90, resize
-from system import loadIplImage, loadNumpyImage, saveNumpyImage, saveKeypoints
